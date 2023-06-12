05a6aa0eb8f8569d1f7a898760ebfa96314e4bd0
Birajit Debbarma <birajitdebbarma@gmail.com>
2023-06-12 20:52:21 +0530

    new pull

--- server/model/database.js
+++ server/model/database.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
 
 
 
- mongoose.connect('mongodb+srv://berean:bereanMark@kokthumnews.fdgowiq.mongodb.net?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+ mongoose.connect('mongodb+srv://berean:bereanMark@kokthumnews.fdgowiq.mongodb.net/kokthumall?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
